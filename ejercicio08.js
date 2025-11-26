114e9de23bc90229f1e0e766da21fb949ec9e9f1
johan57211 <jyucrat@unsa.edu.pe>
2025-11-26 15:26:04 -0500

    ejercicio08

--- ejercicio08.js
+++ ejercicio08.js
@@ -1,17 +1,20 @@
 const btn = document.getElementById("btnUsuario");
 const info = document.getElementById("informacion");
 
-btn.addEventListener("click", () => {
-    fetch("https://jsonplaceholder.typicode.com/users/2")
-    .then (respuesta => respuesta.json())
-    .then(usuario => {
+async function cargarUsuario() {
+    try {
+        const respuesta = await fetch("https://jsonplaceholder.typicode.com/users/2");
+
+        const usuario = await respuesta.json();
+
         info.innerHTML = `
             <p><strong>Nombre:</strong> ${usuario.name}</p>
             <p><strong>Email:</strong> ${usuario.email}</p>
             <p><strong>Ciudad:</strong> ${usuario.address.city}</p>
         `;
-    })
-    .catch (error => {
-        console.error("Ocurrio un error: "+error)
-    })
-})
+    } catch (error) {
+        console.error("Hubo un error:", error);
+    }
+}
+
+btn.addEventListener("click", cargarUsuario);
