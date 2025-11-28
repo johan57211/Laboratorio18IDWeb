e96a30e80d02d4dd43e8e921c4ed5f34b0fc7168
johan57211 <jyucrat@unsa.edu.pe>
2025-11-28 18:16:31 -0500

    ejercicio09

---
+++ ejercicio09/app.js
@@ -0,0 +1,18 @@
+import {suma, resta} from "./sumaResta.js"
+import multiplicar from "./multiplicacionDivision.js"
+import { division } from "./multiplicacionDivision.js"
+
+console.log("La suma es: " + suma(2, 4));
+console.log("La resta es: " + resta(5, 3));
+console.log("La multiplicacion es: "+multiplicar(4, 5));
+try{
+    console.log("La division es: " + division(3, 0));
+} catch (e) {
+    console.error("Error en la division: " + e);
+}
+
+try{
+    console.log("La division es: "+division(9, 3));
+} catch (e) {
+    console.error("Error en la division: " + e);
+}
---
+++ ejercicio09/multiplicacionDivision.js
@@ -0,0 +1,10 @@
+export default function multiplicar (a, b) {
+    return a * b;
+}
+
+export function division (a, b) {
+    if (b === 0){
+        throw new Error ("No se puede dividir entre 0");
+    }
+    return a / b;
+}
